585c88c6d4d57fe91722dadf327baccea809c20c
thernande <tehp1993@gmail.com>
2017-09-29 20:16:16 -0500

    conexion a mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict'
 
-asdsf
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/zoo',{ useMongoClient: true }).then(() => {
+			console.log('la conexion a la base de datos se ha realizado correctamente...');
+	})
+	.catch(err => console.log(err));
