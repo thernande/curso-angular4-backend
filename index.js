379e6e0133c04670f1a34de557fe6ea60a4fb332
thernande <tehp1993@gmail.com>
2017-09-29 22:54:48 -0500

    creacion de model para conectar las tablas de la base de datos

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3789;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/zoo',{ useMongoClient: true }).then(() => {
 			console.log('la conexion a la base de datos se ha realizado correctamente...');
+
+			app.listen(port, () => {
+				console.log("el servidor local con node esta corriendo");
+			})
 	})
 	.catch(err => console.log(err));
